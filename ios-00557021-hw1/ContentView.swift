8968951392c78056ec8ed0bca218610edfafc4ec
jackson <jiechun.lai@gmail.com>
2021-03-10 12:59:11 +0800

    hw1

--- ios-00557021-hw1/ContentView.swift
+++ ios-00557021-hw1/ContentView.swift
@@ -8,10 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number = 0
+    @State private var showAlert = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        ZStack {
+            Image("1")
+                .resizable()
+                .opacity(0.43)
+                
+            VStack {
+                Text("元宵猜燈謎")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.red)
+                    .offset(x:0 ,y:-200)
+                Button(action:{
+                    if number > 9{
+                        number = 0
+                    }
+                    else if number == 0{
+                        question.shuffle()
+                        number = number + 1
+                    }
+                    else{
+                        number = number+1
+                    }
+                    }
+                ) {
+                    if number < 1{
+                    Text("開始遊戲")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.blue)
+                    }
+                    else{
+                        Text("下一題")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.blue)
+                        Button(action: {
+                            showAlert = true
+                        }) {
+                            Text("看答案")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.blue)
+                        }.alert(isPresented: $showAlert){ () -> Alert in
+                            return Alert(title: Text(question[number].ans))
+                        }
+                    }
+                }
+                if number == 0{
+                    Text("準備好了嗎")
+                }
+                else {
+                    Text("第\(number)題")
+                    Text(question[number].ques)
+                }
+        }
     }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ ios-00557021-hw1/question.swift
@@ -0,0 +1,32 @@
+//
+//  question.swift
+//  ios-00557021-hw1
+//
+//  Created by User02 on 2021/3/10.
+//
+
+import Foundation
+
+struct qa {
+    let ques :String
+    let ans :String
+    let id :Int
+}
+
+var question = [
+    qa(ques:"什麼碗打不爛?",ans:"鐵飯碗",id:2),
+    qa(ques:"什麼蛋又能走又能跳還會說話?",ans:"笨蛋",id:3),
+    qa(ques:"最不聽話的是誰?",ans:"聾子",id:4),
+    qa(ques:"為什麼非洲疾病很多？",ans:"因為空腹不能吃藥",id:5),
+    qa(ques:"為什麼得癌症的孩子都很健談",ans:"因為他們都很有“化療”",id:6),
+    qa(ques:"為什麼物理學家霍金考不到駕照？",ans:"因為他沒辦法看左後照鏡",id:7),
+    qa(ques:"牙醫靠什麼吃飯",ans:"嘴巴",id:8),
+    qa(ques:"和尚帶帽子，(猜一成語)?",ans:"無法無天",id:9),
+    qa(ques:"什麼東西天氣越熱，它爬的越高?",ans:"溫度計",id:10),
+    qa(ques:"什麼東西沒腳走天下?",ans:"船",id:11),
+    qa(ques:"什麼書你不可能在書店裡買到?",ans:"秘書",id:12),
+    qa(ques:"什麼人一年只上一天班?",ans:"聖誕老人",id:13),
+    qa(ques:"最細的針?猜一成語。",ans:"無孔不入",id:14),
+    qa(ques:"醫治暈車的最好辦法是什麼?",ans:"不坐車",id:15),
+    qa(ques:"什麼屎不臭?",ans:"天使",id:16),
+]
