043524c2c72f0c421ea2b961002982fa98a02bff
jackson <jiechun.lai@gmail.com>
2021-03-10 10:36:45 +0800

    Initial Commit

---
+++ ios-00557021-hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-00557021-hw1
+//
+//  Created by User02 on 2021/3/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-00557021-hw1/ios_00557021_hw1App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_00557021_hw1App.swift
+//  ios-00557021-hw1
+//
+//  Created by User02 on 2021/3/10.
+//
+
+import SwiftUI
+
+@main
+struct ios_00557021_hw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
